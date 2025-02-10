4a0cf4887fb1f708fe264c19076bd5d46bed7f99
Bhupendra Makode <76209510+BHUPENDRA28D@users.noreply.github.com>
2025-02-10 11:50:52 +0530

    Added okhttp to make httprequests form plain java code.

--- 00.Sanket Singh Course/14.First-Gradle-Project/First_Gradle/src/main/java/Main.java
+++ 00.Sanket Singh Course/14.First-Gradle-Project/First_Gradle/src/main/java/Main.java
@@ -1,9 +1,32 @@
+import okhttp3.OkHttp;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
 
+        String url = "https://jsonplaceholder.typicode.com/todos";
 
-        for(int i=0;i<10;i++){
-            System.out.println(i+1);
+
+        System.out.println("Hello Bhupendra Makode");
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()){
+           if(!response.isSuccessful()){
+               System.out.println("Something want wrong !! ");
+           }
+            System.out.println(response.body().string());
+        }catch (IOException e){
+            e.printStackTrace();
         }
+
     }
 }
