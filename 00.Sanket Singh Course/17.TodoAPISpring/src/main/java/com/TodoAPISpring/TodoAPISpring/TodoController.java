660cedbc9fc36222fb5740315d34f106ebecc577
Bhupendra Makode <76209510+BHUPENDRA28D@users.noreply.github.com>
2025-02-10 23:58:51 +0530

    Post Mapping -added todo creation

--- 00.Sanket Singh Course/17.TodoAPISpring/src/main/java/com/TodoAPISpring/TodoAPISpring/TodoController.java
+++ 00.Sanket Singh Course/17.TodoAPISpring/src/main/java/com/TodoAPISpring/TodoAPISpring/TodoController.java
@@ -1,12 +1,14 @@
 package com.TodoAPISpring.TodoAPISpring;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+@RestController //Mapping automaticaly..
 public class TodoController {
 
     private static List<Todo> todosList;
@@ -23,5 +25,12 @@ public class TodoController {
     public List<Todo> getTodos(){
         return todosList;
     }
+
+    @PostMapping("/todos")
+    public Todo createTodo(@RequestBody Todo newTodo){
+        todosList.add(newTodo);
+        return newTodo;
+    }
+
 
 }
