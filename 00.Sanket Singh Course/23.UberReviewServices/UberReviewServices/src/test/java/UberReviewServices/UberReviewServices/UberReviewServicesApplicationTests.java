ac84cd7ddbfa645626de34521d04e906cde0eba1
Bhupendra Makode <76209510+BHUPENDRA28D@users.noreply.github.com>
2025-02-13 00:22:50 +0530

    Spring data JPA Configuration is done

---
+++ 00.Sanket Singh Course/23.UberReviewServices/UberReviewServices/src/test/java/UberReviewServices/UberReviewServices/UberReviewServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package UberReviewServices.UberReviewServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberReviewServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
