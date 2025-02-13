5d874ccb162cf51311e2b85c34234a041b0ce808
Bhupendra Makode <76209510+BHUPENDRA28D@users.noreply.github.com>
2025-02-13 20:35:33 +0530

    Column LastDateUpdate

--- 00.Sanket Singh Course/23.UberReviewServices/UberReviewServices/src/main/java/UberReviewServices/UberReviewServices/models/Review.java
+++ 00.Sanket Singh Course/23.UberReviewServices/UberReviewServices/src/main/java/UberReviewServices/UberReviewServices/models/Review.java
@@ -1,11 +1,32 @@
 package UberReviewServices.UberReviewServices.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
+import java.util.function.DoubleToLongFunction;
 
 @Entity
+@Table(name = "booking_review")
 public class Review {
 
  @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
  Long id;
+
+@Column(nullable = false)
+ String content;
+
+ Double rating ;
+
+ @Column(nullable = false)
+ @Temporal(TemporalType.TIMESTAMP) // this annotation tells spring about the formats of Date object to be stored ie.Date/Time/Timestamp.
+ @CreatedDate //this annotation tells spring that only handle ir for object cration
+  Date createdAt;
+
+ @Column(nullable = false)
+ @Temporal(TemporalType.TIMESTAMP) // this annotation tells spring about the formats of Date object to be stored ie.Date/Time/Timestamp.
+ @LastModifiedDate //this annotation tells spring that only handles it object update.
+ Date updatedAt;
 }
